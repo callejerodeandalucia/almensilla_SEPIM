a1471e1c412c9b08172b831b4362471f1b419445
Javier G. Leal <jgleal@gmail.com>
2015-08-25 14:40:55 +0200

    configuración

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 url = "http://mapea-sigc.juntadeandalucia.es/sepim_server/api";
-urlCore = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sepim_montefrio/search";
-urlSuggest = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sepim_montefrio/suggest";
-idAplicacion = 33; //id de la aplicación
+urlCore = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sepim_almensilla/search";
+urlSuggest = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sepim_almensilla/suggest";
+idAplicacion = 51; //id de la aplicación
 aplicacion = null;
 pilaCategorias = [];
 coor_x = null;
